2521073c2fda268b3182e9b8364f568665c23161
moumita das <44020214+moumitaguri@users.noreply.github.com>
2019-01-16 21:05:19 +0530

    Initial commit. As per client's requirement following are done.
    Title “Flower Catalog” at the top
    Image of flowers
    Links to pages of Abeliophyllum, Ageratum and a guest book.
    
    nodemon is added as dev-dependencies in package-lock.json

--- src/app.js
+++ src/app.js
@@ -1,6 +1,34 @@
-const app = (req, res) => {
-  res.statusCode = 404;
+const fs = require('fs');
+
+const getFileName = function (url) {
+  if (url == "/") return "./index.html";
+  return "." + url;
+}
+
+const send = function (res, content, statusCode = 200) {
+  res.statusCode = statusCode;
+  res.write(content);
   res.end();
+}
+
+const getDetails = function (path, res) {
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      send(res, "not found", 404);
+      return;
+    }
+    send(res, data, 200);
+  })
+}
+
+const logRequest = function (req) {
+  console.log(req.url, req.method);
+}
+
+const app = (req, res) => {
+  logRequest(req);
+  let path = getFileName(req.url);
+  getDetails(path, res);
 };
 
 // Export a function that can act as a handler
